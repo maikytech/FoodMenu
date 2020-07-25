ef465c626e9502c56a73ef286fbd40b99b9bce6c
maikytech <maikytech1307@gmail.com>
2020-07-25 11:16:06 -0500

    Project Start

---
+++ FoodMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodMenu
+//
+//  Created by Maiqui Cedeño on 25/07/20.
+//  Copyright © 2020 Poseto Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
